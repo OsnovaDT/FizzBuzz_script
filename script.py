50fedb4524b356a448e758c12b9d136e1118c9a9
e.stepanov <maestrojane@gmail.com>
2022-06-22 14:41:17 +0300

    Add replacement_options arg

--- script.py
+++ script.py
@@ -1,20 +1,18 @@
-# 3) Добавить в функцию вариант замены элементов
 # 4) Отрефакторить
 
-def run_fizz_buzz(max_number: int) -> None:
+def run_fizz_buzz(
+        max_number: int, replacement_options={3: 'Fizz', 5: 'Buzz'}) -> None:
     """Run FizzBuzz game"""
 
     for number in range(1, max_number + 1):
         fizz_buzz = ''
 
-        if number % 3 == 0:
-            fizz_buzz += 'Fizz'
-
-        if number % 5 == 0:
-            fizz_buzz += 'Buzz'
+        for key, value in replacement_options.items():
+            if number % key == 0:
+                fizz_buzz += value
 
         print(fizz_buzz or number)
 
 
 if __name__ == '__main__':
-    run_fizz_buzz(16)
+    run_fizz_buzz(28, {4: 'Argh', 7: 'Blergh'})
