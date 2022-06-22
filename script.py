6395610f93db8e5ac99d2785f6a37a80697295e6
e.stepanov <maestrojane@gmail.com>
2022-06-22 14:33:39 +0300

    Add FizzBuzz function

--- script.py
+++ script.py
@@ -1,4 +1,21 @@
-# 1) Реализовать функцию с FizzBuzz
 # 2) Добавить в функцию числовой лимит
 # 3) Добавить в функцию вариант замены элементов
 # 4) Отрефакторить
+
+def run_fizz_buzz() -> None:
+    """Run FizzBuzz game"""
+
+    for number in range(1, 20):
+        fizz_buzz = ''
+
+        if number % 3 == 0:
+            fizz_buzz += 'Fizz'
+
+        if number % 5 == 0:
+            fizz_buzz += 'Buzz'
+
+        print(fizz_buzz or number)
+
+
+if __name__ == '__main__':
+    run_fizz_buzz()
